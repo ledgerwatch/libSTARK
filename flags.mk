a234d645d5b5c62b1a9dbf1375cbefff04825266
Vladimir A. Nosenko <vn@cypodgroup.com>
2019-04-30 15:19:12 +0200

    flags.mk: Added support for Linux and Darwin platforms

--- flags.mk
+++ flags.mk
@@ -1,5 +1,21 @@
-#CC=/usr/local/bin/g++-8
-#CC=clang++
+OSFLAG 				:=
+ifeq ($(OS),Windows_NT)
+	# TODO
+else
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Linux)
+		CC=/usr/bin/g++
+		AR=/usr/bin/ar crv
+	endif
+	
+	ifeq ($(UNAME_S),Darwin)
+		# Homebrew: gcc v8
+		CC=/usr/local/bin/g++-8
+		# XCode libtool instead of gnu ar
+		AR=libtool -static -a -o
+	endif
+endif
+# Default: AR=
 CPPFLAGS=-std=c++11
 CFLAGS=-O3 -g -Wall -fmessage-length=0 -maes -msse4 -mtune=native
 CFLAGS+=-fopenmp
